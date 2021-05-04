4c437b21f40dceb1b4c8d04913db7e9865f050e6
William Newell <newellwm@gmail.com>
2021-05-04 00:33:49 -0500

    Add missing read_word header guard

--- cpp/accelerated-cpp/chapter_04/read_words-4-5.h
+++ cpp/accelerated-cpp/chapter_04/read_words-4-5.h
@@ -1,5 +1,9 @@
+#ifndef GUARD_read_words_h
+#define GUARD_read_words_h
+
 #include <iostream>
 #include <string>
 #include <vector>
 
 std::istream& read_words(std::istream& in, std::vector<std::string>& words);
+#endif
