121883eb531248037537702a23f450492abbfbb4
William Newell <newellwm@gmail.com>
2021-05-20 21:50:47 -0500

    Add split and extract_fails examples

--- cpp/accelerated-cpp/chapter_05/Student_info.cc
+++ cpp/accelerated-cpp/chapter_05/Student_info.cc
@@ -1,10 +1,14 @@
 #include "Student_info.h"
+#include "grade.h"
 
 #include <iostream>
 #include <vector>
+#include <list>
 
 using std::istream;
 using std::vector;
+using std::list;
+typedef vector<Student_info>::size_type students_size;
 
 bool compare(const Student_info& x, const Student_info& y) {
   return x.name < y.name;
@@ -27,7 +31,49 @@ istream& read_hw(istream& in, vector<double>& hw) {
   return in;
 }
 
+list<Student_info> extract_fails(list<Student_info>& students) {
+  list<Student_info> fail;
+  list<Student_info>::iterator iter = students.begin();
+  while(iter != students.end()) {
+    if(fgrade(*iter)) {
+      fail.push_back(*iter);
+      iter = students.erase(iter);
+    } else
+      ++ iter;
+  }
+  return fail;
+}
+
 vector<Student_info> extract_fails(vector<Student_info>& students) {
+  vector<Student_info> fail;
+  vector<Student_info>::iterator iter = students.begin();
+  while(iter != students.end()) {
+    if(fgrade(*iter)) {
+      fail.push_back(*iter);
+      iter = students.erase(iter);
+    } else
+      ++ iter;
+  }
+  return fail;
+}
+
+
+vector<Student_info> even_worse_extract_fails(vector<Student_info>& students) {
+  vector<Student_info> fail;
+  students_size i = 0;
+  while(i != students.size()) {
+    if (fgrade(students[i])) {
+      fail.push_back(students[i]);
+      students.erase(students.begin() + i);
+    } else {
+      ++i;
+    }
+  }
+  return fail;
+}
+
+// requires 2N storage because of the array copy, can do better
+vector<Student_info> copy_extract_fails(vector<Student_info>& students) {
   vector<Student_info> pass, fail;
   for(Student_info& student : students) {
     if (fgrade(student))
@@ -37,4 +83,4 @@ vector<Student_info> extract_fails(vector<Student_info>& students) {
   }
   students = pass;
   return fail;
-}  
+}
---
+++ cpp/accelerated-cpp/chapter_05/split.h
@@ -0,0 +1,10 @@
+#ifndef GUARD_split_h
+#define GUARD_split_h
+
+#include <string>
+#include <vector>
+#include <cctype>
+
+std::vector<std::string> split(const std::string& s);
+#endif
+
