73672cf64962b1a60b1e5f90a6b141bc66b627f1
William Newell <newellwm@gmail.com>
2021-06-25 23:30:04 -0500

    Fix naming error in concat example

--- cpp/accelerated-cpp/chapter_05/concat.h
+++ cpp/accelerated-cpp/chapter_05/concat.h
@@ -6,6 +6,6 @@
 
 
 std::vector<std::string> vcat(const std::vector<std::string>& top, const std::vector<std::string>& bottom);
-std::vector<std::string> vcat(const std::vector<std::string>& left, const std::vector<std::string>& right);
+std::vector<std::string> hcat(const std::vector<std::string>& left, const std::vector<std::string>& right);
 
 #endif
