2614675e7b4166833fe6cb2bc17ee504924f6142
William Newell <newellwm@gmail.com>
2021-06-26 00:06:18 -0500

    Simplify concat.cc

--- cpp/accelerated-cpp/chapter_05/concat.cc
+++ cpp/accelerated-cpp/chapter_05/concat.cc
@@ -9,10 +9,15 @@ using std::vector;
 using std::string;
 
 vector<string> vcat(const vector<string>& top, const vector<string>& bottom) {
+  // copy all of top into ret
   vector<string> ret = top;
-  for(vector<string>::const_iterator iter = bottom.begin(); iter != bottom.end(); ++iter) {
-    ret.push_back(*iter);
-  }
+
+  // append bottom to the end of ret
+  //  Equivalent to:
+  //  for(vector<string>::const_iterator iter = bottom.begin(); iter != bottom.end(); ++iter) {
+  //    ret.push_back(*iter);
+  //  }
+  ret.insert(ret.end(), bottom.begin(), bottom.end());
   return ret;
 }
 
